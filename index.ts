ea83d6cb964826fffcc9a2b77bfdf6b61fb5e276
Austin Wasson <austinpwasson@gmail.com>
2023-12-03 15:59:59 -0700

    update file tree

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { openPullRequests } from './src/services/github/pull-requests';
+import { openPullRequests } from './src/github/pull-requests'
 
 const response = await openPullRequests()
 
