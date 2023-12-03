b01442d9f1a16ca90739b1474a288b7919be1614
Austin Wasson <austinpwasson@gmail.com>
2023-12-03 14:38:41 -0700

    octokit; dotenv; readme

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hey from Tailor 🤖");
+import 'dotenv/config'
+import { openPullRequests } from './src/services/github/pull-requests';
+
+const response = await openPullRequests()
+
+// Example response for command: tailorai -l
+response.data.forEach(repo => {
+  console.log(`${repo.head.ref} #${repo.number} - ${repo.title}`)
+})
