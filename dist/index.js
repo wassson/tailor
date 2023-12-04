788ed18b5c7da3724fa87aeae8ba5278eb7e2f6f
Austin Wasson <austinpwasson@gmail.com>
2023-12-03 20:38:21 -0700

    initial setup of cli

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env bun
+import 'dotenv/config';
+import { openPullRequests } from './github';
+const { Command } = require("commander"); // add this line
+const program = new Command();
+program
+    .version("1.0.0-alpha.1")
+    .description("An AI code reviewer for GitHub pull requests")
+    .option("-l, --ls", "List open pull requests")
+    .option("-r <value>, --review <value>", "Review pull request")
+    .option("-d <value>, --describe <value>", "Describe pull request")
+    .parse(process.argv);
+const options = program.opts();
+if (options.ls) {
+    const response = await openPullRequests();
+    // Example response for command: tailorai -l
+    response.data.forEach((repo) => {
+        console.log(`${repo.head.ref} #${repo.number} - ${repo.title}`);
+    });
+}
+if (options.review || options.r) {
+    console.log("review");
+}
+if (options.describe || options.d) {
+    console.log("describe");
+}
+//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import { openPullRequests } from './src/github'
-
-const response = await openPullRequests()
-
-// Example response for command: tailorai -l
-response.data.forEach(repo => {
-  console.log(`${repo.head.ref} #${repo.number} - ${repo.title}`)
-})
